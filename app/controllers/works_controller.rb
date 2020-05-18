e8ad8016a73da510c3d268fb55f4fd2a90a57dd0
Stephanie <stephaniejmars@gmail.com>
2020-05-17 17:23:00 -0700

    works edit, update, destroy added. flash view updated

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -29,12 +29,43 @@ class WorksController < ApplicationController
   end
 
   def edit
+    @work = Work.find_by(id: params[:id])
+
+    if @work.nil?
+      head :not_found
+      return
+    end
   end
 
   def update
+    @work = Work.find_by(id: params[:id])
+    if @work.nil?
+      head :not_found
+      return
+    elsif @work.update(work_params)
+      redirect_to work_path(@work.id)
+      flash[:success] = "Work Successfully Updated"
+      return
+    else 
+      render :edit, status: :bad_request
+      flash.now[:error] = "Failed to Update Work"
+      return
+    end
   end
 
   def destroy
+    id = params[:id].to_i
+    @work = Work.find_by(id: id)
+
+    if @work.nil?
+      head :not_found
+      return
+    end
+    
+    @work.destroy
+    redirect_to works_path 
+    flash[:success] = "Work Successfully Deleted"
+    return
   end
 
   private
